8ece8454e69abf536d4220fa63326b28de66a3d0
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2024-05-03 06:28:17 +0600

    redux middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './rootReducer';
 
-export const store = createStore(rootReducer);
+const customLogger =
+  ({ getState, dispatch }) =>
+  (next) =>
+  (action) => {
+    console.log(`action: ${JSON.stringify(action)}`);
+    console.log(`current: ${JSON.stringify(getState())}`);
+
+    console.log(
+      `next state: ${JSON.stringify([action].reduce(rootReducer, getState()))}`
+    );
+
+    next(action);
+  };
+
+export const store = createStore(rootReducer, applyMiddleware(customLogger));
