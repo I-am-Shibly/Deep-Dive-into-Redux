a607723231287e7821d1504e50d5fae31968d52a
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2024-05-03 16:49:32 +0600

    added devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import {rootReducer} from './rootReducer';
+import { rootReducer } from './rootReducer';
 import { customLogger } from './customLogger/logger';
-import Logger from 'redux-logger'
+import Logger from 'redux-logger';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-export const store = createStore(rootReducer, applyMiddleware(customLogger, Logger));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(customLogger, Logger))
+);
